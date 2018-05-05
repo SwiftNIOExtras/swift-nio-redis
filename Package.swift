f9f2d286d9308d71a78dd071e7a7e59307dbd2b2
Helge Hess <me@helgehess.eu>
2018-05-05 12:08:35 +0200

    Refer to NIO master branch

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-nio.git", 
-                 from: "1.5.0"),
+                 .branch("master")),
     ],
     targets: [
         .target(name: "NIORedis", dependencies: [ "NIO", "NIOFoundationCompat" ]),
