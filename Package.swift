b191d630fd328054fef84676f7a39a7f8651c5e2
Helge Heß <devteam@zeezide.de>
2019-03-26 18:57:17 +0100

    Whitespace ...
    ...

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "swift-nio-redis",
     products: [
-        .library   (name: "NIORedis", targets: [ "NIORedis" ]),
+        .library(name: "NIORedis", targets: [ "NIORedis" ]),
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-nio.git", 
