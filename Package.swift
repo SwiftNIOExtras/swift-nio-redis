ad86e6ca55f1d3c8984107636494af4f5083e1be
Helge Heß <devteam@zeezide.de>
2019-03-26 18:56:25 +0100

    Update NIO version to 1.13.2
    ... not 2 yet, probably need a branch for this.

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-nio.git", 
-                 from: "1.8.0"),
+                 from: "1.13.2"),
     ],
     targets: [
         .target(name: "NIORedis", 
